e50aede6031fc0c5e28dee64ebeaa2699132a391
Teddy K <teddykoerniadi@gmail.com>
2023-09-09 14:35:04 +0700

    crud method

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -16,6 +16,50 @@ class BaseController {
       next(error)
     }
   }
+
+  create = async (req, res, next) => {
+    try {
+      const data = await this.model.create(req.body)
+
+      let responseSuccess = responseHelper.success(data, 'Data successfully created', 201)
+
+      res.status(responseSuccess.code).json(responseSuccess)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  update = async (req, res, next) => {
+    try {
+      const data = await this.model.update(req.body, {
+        where: {
+          id: req.params.id
+        }
+      })
+
+      let responseSuccess = responseHelper.success(data, 'Data successfully updated', 200)
+
+      res.status(responseSuccess.code).json(responseSuccess)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  destroy = async (req, res, next) => {
+    try {
+      const data = await this.model.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+
+      let responseSuccess = responseHelper.success(data, 'Data successfully removed', 204)
+
+      res.status(responseSuccess.code).json(responseSuccess)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 module.exports = BaseController
