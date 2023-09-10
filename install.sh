9d969f611ad4f3ce893aca824cab3bebff91206d
Teddy K <teddykoerniadi@gmail.com>
2023-09-10 21:12:29 +0700

    create install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+echo "Checking Installation Requirements"
+
 if ! command -v git >/dev/null; then
   echo "Git is not installed on your machine" 
 
@@ -18,7 +21,19 @@ if ! command -v npm >/dev/null; then
   exit 1
 fi
 
+echo "Installation Requirements met"
+echo "Installing boilerplate-node"
 git clone git@github.com:tedante/boilerplate-node.git
 cd boilerplate-node 
 
-docker compose up -d --build
+echo "Installing dependencies"
+if command -v pnpm >/dev/null; then
+  pnpm install
+else 
+  npm install
+fi
+
+echo "Creating .env file"
+cp .env.example .env
+
+echo "Installation complete"
