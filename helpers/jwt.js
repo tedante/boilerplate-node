38f357e44774ffafdfde9314f28ca70135cd996e
Teddy K <teddykoerniadi@gmail.com>
2023-09-12 20:17:30 +0700

    jwt xhange to arrow function

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,9 +1,7 @@
 module.exports = {
-  generateJWT({ payload }) {
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+  generateJWT: ({ payload }) => {
+    return jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: process.env.JWT_EXPIRE,
     });
-
-    return token;
   }
 }
