51763b90c1d353d8b1b5a63654e5b21d94955267
Teddy K <teddykoerniadi@gmail.com>
2023-11-01 15:37:21 +0700

    relation user and role

--- models/user.js
+++ models/user.js
@@ -14,7 +14,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.belongsTo(models.Role, {
+        foreignKey: "RoleId",
+      })
     }
 
     validPassword(password) {
