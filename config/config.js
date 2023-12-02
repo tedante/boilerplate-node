9d67a0bdb348e6087954ba54041fdccff759ef4a
Teddy K <teddykoerniadi@gmail.com>
2023-12-02 20:28:58 +0700

    Add Sequelize configuration file and update database configurations

--- config/config.js
+++ config/config.js
@@ -9,6 +9,15 @@ const sequelizeConfig = {
     database: process.env.DB_NAME || "development-db",
     dialect: process.env.DB_DRIVER || "postgres",
     logging: process.env.DB_LOGGING || false,
+  },
+  test: {
+    host: process.env.DB_TEST_HOST || "localhost",
+    port: process.env.DB_TEST_PORT || "5432",
+    username: process.env.DB_TEST_USERNAME || "postgres",
+    password: process.env.DB_TEST_PASSWORD || "postgres",
+    database: process.env.DB_TEST_NAME || "test-db",
+    dialect: process.env.DB_TEST_DRIVER || "postgres",
+    logging: process.env.DB_TEST_LOGGING || false,
   }
 }
 
