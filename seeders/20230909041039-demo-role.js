21a30ce06d6dde132bc639f104cf87c11b93d15e
Teddy K <teddykoerniadi@gmail.com>
2023-12-02 15:04:26 +0700

    refactor seeder code,  move data to json

--- seeders/20230909041039-demo-role.js
+++ seeders/20230909041039-demo-role.js
@@ -4,22 +4,15 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
    // create bulk insert data to table role
-    await queryInterface.bulkInsert('Roles', [
-      {
-        id: "550b3cc1-0551-420f-872d-91f0181201b6",
-        name: 'admin',
-        description: 'Administrator',
+    let roles = require('./data/roles.json').map(role => {
+      return {
+        ...role,
         createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        id: "0209cbd3-2069-44a7-92a2-20af22dfe8ff",
-        name: 'user',
-        description: 'User',
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        updatedAt: new Date()
       }
-    ]);
+    })
+
+    await queryInterface.bulkInsert('Roles', roles, {});
   },
 
   async down (queryInterface, Sequelize) {
